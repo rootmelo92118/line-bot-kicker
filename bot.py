a76d25ac3ac283faca3cd3b3e1516cfb245c1d20
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2018-05-28 21:08:42 +0800

    Update and rename bot1.py to bot.py

---
+++ bot.py
@@ -0,0 +1,79 @@
+from linepy import *
+
+
+client = LINE()
+client.log("Auth Token : " + str(client.authToken))
+
+oepoll = OEPoll(client)
+
+MySelf = client.getProfile()
+JoinedGroups = client.getGroupIdsJoined()
+print("My MID : " + MySelf.mid)
+
+whitelist = ["mid1","mid2"]
+
+
+def NOTIFIED_INVITE_INTO_GROUP(op):
+    try:
+        if op.param1 not in JoinedGroups:
+            if op.param2 in whitelist:
+                client.acceptGroupInvitation(op.param1)
+                JoinedGroups.append(op.param1)
+                group = client.getGroup(msg.to)
+                targets = []
+                for contact in group.members:
+                    targets.append(contact.mid)
+                    targets.remove(whitelist)
+                if targets == []:
+                    client.leaveGroup(op.param1)
+                    JoinedGroups.remove(op.param1)
+                else:
+                    for target in targets:
+                        try:
+                            klist=[client]
+                            kicker=random.choice(klist)
+                            kicker.kickoutFromGroup(msg.to,[target])
+                            print(op.param1, [contact.mid])
+                        except:
+                            group.name = ""
+                            client.updataGroup(group)
+                            client.leaveGroup(op.param1)
+                            JoinedGroups.remove(op.param1)
+            else:
+                client.acceptGroupInvitation(op.param1)
+                JoinedGroups.append(op.param1)
+                client.inviteIntoGroup(op.param1, ["mid1"])
+                group = client.getGroup(msg.to)
+                targets = []
+                for contact in group.members:
+                    targets.append(contact.mid)
+                    targets.remove(whitelist)
+                    targets.remove(op.param2)
+                if targets == []:
+                    client.leaveGroup(op.param1)
+                    JoinedGroups.remove(op.param1)
+                else:
+                    for target in targets:
+                        try:
+                            klist=[client]
+                            kicker=random.choice(klist)
+                            kicker.kickoutFromGroup(msg.to,[target])
+                            print(op.param1, [contact.mid])
+                        except:
+                            group.name = ""
+                            client.updataGroup(group)
+                            client.leaveGroup(op.param1)
+                            JoinedGroups.remove(op.param1)
+    except Exception as e:
+        print(e)
+        print("\n\nNOTIFIED_INVITE_INTO_GROUP\n\n")
+        return
+    
+    
+oepoll.addOpInterruptWithDict({
+    OpType.NOTIFIED_INVITE_INTO_GROUP: NOTIFIED_INVITE_INTO_GROUP
+})
+
+
+while True:
+    oepoll.trace()

28c9c1511cd6d663c9d20f698d1a862763963a19
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2018-05-29 17:34:56 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,88 +18,47 @@ def NOTIFIED_INVITE_INTO_GROUP(op):
         if op.param1 not in JoinedGroups:
                 client.acceptGroupInvitation(op.param1)
                 JoinedGroups.append(op.param1)
-                group = client.getGroup(op.param1)
-                for contact in group.members:
-                    targets.append(contact.mid)
-                if targets == []:
-                    client.leaveGroup(op.param1)
-                    JoinedGroups.remove(op.param1)
-                else:
-                        try:
-                            kick = [targets]
-                            klist = random.choice(kick)
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            client.kickoutFromGroup(op.param1, [klist])
-                            print(op.param1, [contact.mid])
-                        except:
-                            group.name = "血盟に荣光あれ☆彡"
-                            client.updateGroup(group)
-                            client.leaveGroup(op.param1)
-                            JoinedGroups.remove(op.param1)
+                client.sendMessage(op.param1, "血盟に荣光あれ☆彡")
     except Exception as e:
         print(e)
         print("\n\nNOTIFIED_INVITE_INTO_GROUP\n\n")
         return
     
     
-def KICKOUT_FROM_GROUP(op):
+def SEND_MESSAGE(op):
+    msg = op.message
     try:
-        targets.remove(op.param3)
-    except Exception as e:
-        print(e)
-        print("\n\nKICKOUT_FROM_GROUP\n\n")
-        return
-    
+        if msg.toType == 2:
+            if msg.contentType == 0:
+                if msg.text == "血盟に荣光あれ☆彡":
+                    print "start destroying"
+                    _name = msg.text.replace("血盟に荣光あれ☆彡","")
+                    group = client.getGroup(msg.to)
+                    targets = []
+                    for g in group.members:
+                        if _name in g.displayName:
+                            targets.append(g.mid)
+                    if targets == []:
+                        client.leaveGroup(msg.to)
+                        JoinedGroups.remove(msg.to)
+                    else:
+                        for target in targets:
+                            try:
+                                klist=[client]
+                                kicker=random.choice(klist)
+                                kicker.kickoutFromGroup(msg.to,[target])
+                                print (msg.to,[g.mid])
+                            except:
+                               group.name = "血盟に荣光あれ☆彡"
+                               client.updateGroup(group)
+                               client.leaveGroup(op.param1)
+                               JoinedGroups.remove(op.param1)
+        else:
+            pass
     
 oepoll.addOpInterruptWithDict({
     OpType.NOTIFIED_INVITE_INTO_GROUP: NOTIFIED_INVITE_INTO_GROUP,
-    OpType.KICKOUT_FROM_GROUP: KICKOUT_FROM_GROUP
+    OpType.SEND_MESSAGE: SEND_MESSAGE
 })
 
 
